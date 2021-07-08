e40deb6c0194e695f180775b4b31f66985740596
diego martinez <juandiego337@hotmail.com>
2021-07-08 15:58:56 -0500

    First commit of the project

---
+++ src/main/java/com/sofka/CarGame/CarGameApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.CarGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/CarGame/CarGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.CarGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
