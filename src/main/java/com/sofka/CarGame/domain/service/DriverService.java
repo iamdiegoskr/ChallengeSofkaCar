ad61f3a7ee2548b89ef3e86c21fe659ad37e872a
diego martinez <juandiego337@hotmail.com>
2021-07-09 17:27:29 -0500

    Driver controller is ok

--- src/main/java/com/sofka/CarGame/domain/service/DriverService.java
+++ src/main/java/com/sofka/CarGame/domain/service/DriverService.java
@@ -1,10 +1,36 @@
 package com.sofka.CarGame.domain.service;
 
+import com.sofka.CarGame.domain.model.Driver;
+import com.sofka.CarGame.domain.repository.IDriverRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DriverService {
 
+    @Autowired
+    private IDriverRepository repository;
 
+    public List<Driver> getDrivers(){
+        return repository.getDrivers();
+    }
+
+    public Optional<Driver> getDriverById(int id){
+        return repository.getDriver(id);
+    }
+
+    public Driver save(Driver driver){
+        return  repository.save(driver);
+    }
+
+    public boolean delete(int id){
+        return getDriverById(id).map(driver -> {
+            repository.delete(id);
+            return true;
+        }).orElse(false);
+    }
 
 }
--- src/main/java/com/sofka/CarGame/web/controller/DriverController.java
+++ src/main/java/com/sofka/CarGame/web/controller/DriverController.java
@@ -1,11 +1,28 @@
 package com.sofka.CarGame.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sofka.CarGame.domain.model.Driver;
+import com.sofka.CarGame.domain.service.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/player")
 public class DriverController {
 
+    @Autowired
+    private DriverService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Driver> getDriver(@PathVariable() int idDriver){
+        return service.getDriverById(idDriver).map(driver -> new ResponseEntity<>(driver, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Driver> createDriver(Driver driver){
+        return new ResponseEntity<>(service.save(driver),HttpStatus.CREATED);
+    }
 
 }
